1e6694aa2330975289ec67b1b3830882c87cc6e4
Aadhiseshan <aadhideveloper2000@gmail.com>
2022-07-29 15:27:46 +0530

    Update new python File

---
+++ py-basics.py
@@ -0,0 +1,109 @@
+##print("****************Arithmetic,Logic & Conditional Operators*****************")
+
+##print(5+5)
+##
+##print(5*(2-7))
+##
+##print(5*5)
+##
+##print(5**5)
+##
+##print(40/2)
+##
+##print(40//2)
+##
+##print(100%2)
+##
+##print(10==10)
+##
+##print(5<4)
+##
+##print(5>4)
+##
+##print(5<=4)
+##
+##print(5>=4)
+##
+##no=5
+##
+##print(no)
+
+#====================================================================================================#
+##Implement String Operation
+
+##word=input("Enter the Any Word: ")
+##word1=word[::-1]
+
+##if word == word1:
+##    print(word," is  a palindrome")
+##else:
+##    print(word," is not palindrome")
+
+##file_location=r"\nome\tadhiseshan\GitHub_Learn"
+##print(file_location)
+
+#=====================================================================================================#
+
+#slice operator
+
+##name='ramachandran'
+##print('Your name Length is: '+str(len(name)))
+
+####print(name[0])
+####print(name[1])
+####print(name[2])
+####print(name[3])
+####print(name[4])
+####print(name[5])
+####print(name[6])
+####print(name[7])
+####print(name[8])
+####print(name[9])
+####print(name[10])
+####print(name[11])
+####print(name[12])
+##print(name[0].upper()+name[1:4]+name[4].upper()+name[5:len(name)-1]+name[-1].upper())
+##
+
+#=====================================================================================================#
+
+#step operator
+
+##name='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+##
+##print(len(name))
+##
+##print(name[0:len(name):2])
+
+#=====================================================================================================#
+#title() function
+
+##The title() function is usetd to any one sentence or title type to lower case
+##   then it is converted only first letter in title for ex blew 
+
+##news="government annonced the scheam"
+
+##print(news)
+##print(news.title())
+
+#=====================================================================================================#
+
+##name='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+##
+##print('A' and'KLMNO' in name)
+
+#=====================================================================================================#
+
+#Sorted() Funcution
+
+## In this function is used for check the value Acending order after print Ascending order method.
+##given Example blow 
+
+##word1='swathi Akka'
+##word2='Akka swathi'
+##print(sorted(word1) == sorted(word2))
+##
+##print(sorted(word1))
+
+#=====================================================================================================#
+
